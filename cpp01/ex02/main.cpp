ae77d8a7a29a6b7cf9e221f94205c8b83e47c4a7
Ayoub Lamini <alamini@e2r9p3.1337.ma>
2025-01-27 18:14:19 +0100

    Done

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alamini <alamini@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 17:49:53 by alamini           #+#    #+#             */
+/*   Updated: 2025/01/27 18:04:44 by alamini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+    std::string my_string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &my_string;
+    std::string &stringREF = my_string;
+    
+    std::cout << "my_string address: " << &my_string << std::endl;
+    std::cout << "stringPTR address: " << stringPTR << std::endl; 
+    std::cout << "stringREF address: " << &stringREF << std::endl;
+    std::cout << std::endl;
+    std::cout << "my_string value: " << my_string << std::endl;
+    std::cout << "value pointed by stringPTR : " << *stringPTR << std::endl; 
+    std::cout << "value pointed by stringREF: " << stringREF << std::endl; 
+    
+}

7cc390a77c2d4f7760b633ea51ac3c2d4e93762d
Ayoub Lamini <alamini@e2r9p3.1337.ma>
2025-02-01 16:40:53 +0100

    done

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alamini <alamini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 15:37:18 by alamini           #+#    #+#             */
-/*   Updated: 2025/02/01 16:22:25 by alamini          ###   ########.fr       */
+/*   Updated: 2025/02/01 16:40:03 by alamini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,5 @@ int main()
 {
     Harl Harl;
     Harl.complain("error");
+    return (0);
 }

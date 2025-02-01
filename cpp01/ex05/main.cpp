60ccaba03ea4c521be2d4126db3e259a88d2e2ac
Ayoub Lamini <alamini@e2r9p3.1337.ma>
2025-02-01 17:22:48 +0100

    updated main.cpp

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alamini <alamini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 15:37:18 by alamini           #+#    #+#             */
-/*   Updated: 2025/02/01 16:40:03 by alamini          ###   ########.fr       */
+/*   Updated: 2025/02/01 17:22:13 by alamini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ int main()
 {
     Harl Harl;
     Harl.complain("error");
+    Harl.complain("ERROR");
+    Harl.complain("INFO");
+    Harl.complain("err");
+
     return (0);
 }
